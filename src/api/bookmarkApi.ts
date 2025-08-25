3e7658d2b5b72654fd56de34acbf7bbcff148dc8
minji_105 <ajhjmj9@naver.com>
2025-08-26 00:55:59 +0900

    ✨ Feat: 찜한 스케줄 목록 조회 api 함수 추가 (#178)

--- src/api/bookmarkApi.ts
+++ src/api/bookmarkApi.ts
@@ -3,6 +3,7 @@ import { type AxiosResponse } from 'axios';
 import type {
   BookmarkGroup,
   BookmarkIdol,
+  BookmarkSchedule,
   PaginatedResponse,
 } from '@/types/bookmark';
 
@@ -76,4 +77,23 @@ export const addBookmarkIdol = async (idolId: number) => {
  */
 export const removeBookmarkIdol = async (bookmarkId: number) => {
   await axiosInstance.delete(`/bookmarks/idols/${bookmarkId}/`);
+};
+
+/**
+ * 내가 북마크한 스케줄 목록 전체 조회
+ */
+export const getBookmarkSchedules = async () => {
+  let allSchedules: BookmarkSchedule[] = [];
+  let url: string | null = '/schedules/my/';
+
+  while (url) {
+    // eslint-disable-next-line no-await-in-loop
+    const response: AxiosResponse = await axiosInstance.get<
+      PaginatedResponse<BookmarkSchedule>
+    >(url!);
+    allSchedules = allSchedules.concat(response.data.results);
+    url = response.data.next;
+  }
+
+  return allSchedules;
 };
