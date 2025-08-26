173937e58c0a492b268b7378419945ef6cf1eb2f
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-08-26 21:06:40 +0900

    ♻️ Refactor: 채팅 관련 api 추가 및 수정 (#159)
    채팅방 생성 api 파라미터 수정 / 특정 그룹의 아이돌, 매니저 id 조회하는 api 추가

--- src/api/chatApi.ts
+++ src/api/chatApi.ts
@@ -7,9 +7,13 @@ export const getMyChatRoomListAPI = async () => {
   return res.data;
 };
 
-export const createNewChatRoomAPI = async (groupName: string) => {
+export const createNewChatRoomAPI = async (
+  id: number,
+  participantIds: number[],
+) => {
   const res = await axiosInstance.post(`${API_BASE_URL}/chats/rooms/`, {
-    room_name: `${groupName}Room`,
+    room_name: `ChatRoomBy${id}`,
+    participant_ids: participantIds,
   });
   return res.data;
 };
@@ -86,5 +90,10 @@ export const getChatRoomParticipantsAPI = async (
   const res = await axiosInstance.get(
     `${API_BASE_URL}/chats/rooms/${roomId}/participants/?page=${page}`,
   );
+  return res.data;
+};
+
+export const getGroupMembersAPI = async () => {
+  const res = await axiosInstance.get(`${API_BASE_URL}/groups/members/`);
   return res.data;
 };
